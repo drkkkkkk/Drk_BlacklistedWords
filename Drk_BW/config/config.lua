b6b14627253e3fa5d905f0b2886b6b72ce31b217
drk playz <93538336+drkkkkkk@users.noreply.github.com>
2024-03-04 15:31:17 +0000

    Added main file
    Enjoy saying blacklisted words.... Or well.... Hopefully not!

---
+++ Drk_BW/config/config.lua
@@ -0,0 +1,8 @@
+Config = {}
+
+Config.Webhook = "YourWebHookHere" -- Your discord webhook that the Screenshot / Message will be sent too
+Config.KicKMessage = "Sorry, you have been caught saying a blacklisted word. This has been sent to our staff team VIA A Discord webhook. - You will be punished 😈" -- The message that will appear when the Player is Kicked for saying a no no word.
+
+Config.DiscordAvatarUrl = "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" -- The link for the Discord webhook avatar URL
+
+Config.WebhookName = "Blacklisted Words" -- The name of the Webhook ( Instead of Captain Hook! )
---
+++ Drk_BW/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'John Doe <j.doe@example.com>'
+description 'Example resource'
+version '1.0.0'
+
+server_scripts {
+    'config/config.lua',
+    'server/server.lua',
+
+    
+}
