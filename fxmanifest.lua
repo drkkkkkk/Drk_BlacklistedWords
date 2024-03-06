0967db47193cb33c04f4ab8018d5967b09b9ec87
drk playz <93538336+drkkkkkk@users.noreply.github.com>
2024-03-06 00:36:21 +0000

    Added Version Checker
    Added the functionality of a version checker to ensure you have the correct version of the script!

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'John Doe <j.doe@example.com>'
+description 'Example resource'
+version '1.0.0'
+
+server_scripts {
+
+    'config/config.lua',
+    'server/server.lua',
+    'versioncheck.lua',
+    
+}
